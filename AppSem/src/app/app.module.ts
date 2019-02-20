447667a8423d6bec78c31f8124ecb4da1a37d168
Jonathan <jonatgadielsil@gmail.com>
2019-02-20 04:35:03 -0600

    ultimos cambios

--- AppSem/src/app/app.module.ts
+++ AppSem/src/app/app.module.ts
@@ -6,10 +6,24 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MynavComponent } from './mynav/mynav.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatTableModule } from '@angular/material';
 import { LoginComponent } from './login/login.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { ContactoComponent } from './contacto/contacto.component';
+import { MatCardModule } from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import { CarreraComponent } from './carrera/carrera.component';
+import { ContratoComponent } from './contrato/contrato.component';
+import { DocenteComponent } from './docente/docente.component';
+import { GrupoComponent } from './grupo/grupo.component';
+import { ModuloComponent } from './modulo/modulo.component';
+import { AsistenciaComponent } from './asistencia/asistencia.component';
+import {Component} from '@angular/core';
+import {MatTableDataSource} from '@angular/material';
+import { MatGridListModule } from '@angular/material/grid-list';
+
+
 
 @NgModule({
   declarations: [
@@ -17,7 +31,13 @@ import { ContactoComponent } from './contacto/contacto.component';
     MynavComponent,
     LoginComponent,
     InicioComponent,
-    ContactoComponent
+    ContactoComponent,
+    CarreraComponent,
+    ContratoComponent,
+    DocenteComponent,
+    GrupoComponent,
+    ModuloComponent,
+    AsistenciaComponent
   ],
   imports: [
     BrowserModule,
@@ -25,10 +45,14 @@ import { ContactoComponent } from './contacto/contacto.component';
     BrowserAnimationsModule,
     LayoutModule,
     MatToolbarModule,
+    MatTooltipModule,
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    MatCardModule,
+    MatInputModule,
+    MatTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
